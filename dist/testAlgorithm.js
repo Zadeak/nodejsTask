8708536039a050a70366fa64f4e8259a191672f2
Zadeak <m.jevokimov@sauleit.com>
2021-07-26 19:11:40 +0300

    alg

--- dist/testAlgorithm.js
+++ dist/testAlgorithm.js
@@ -1,41 +1,50 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.test = void 0;
-var firstOne = { num: 1, linkedArray: [2, 3] };
+var firstOne = { num: "1", linkedArray: ["1", "1", "1"] };
 function test() {
-    var depth = 1;
-    console.log(depth);
+    //temp value
+    var number = "2";
+    var depth = 0;
+    var tempListSize = 0;
+    // console.log(depth);
     var nodeList = [firstOne];
     var tempList = [];
-    while (depth != 0) {
-        var prev = 0;
-        var current = nodeList.length;
-        console.log(prev);
-        for (var edge in nodeList.slice(prev)) {
+    while (depth != 3) {
+        // console.log("depth: " + depth);
+        for (var edge in nodeList.slice(-tempListSize)) {
             //   var nodeEdge = nodeList[edge];
             //   nodeEdge.label = "first";
             // получить список всех путей из кода
             var extractedRoutes = nodeList[edge].linkedArray;
+            // console.log("extractedRoute: " + nodeList[edge].linkedArray);
             for (var route in extractedRoutes) {
+                // console.log("route: " + route);
                 // какая-то работа с путями
                 // делаем edge | из edge -> route + weight
-                var newOne = { num: 2, linkedArray: [4, 5] };
+                var newOne = {
+                    num: number,
+                    linkedArray: [number, number, number],
+                };
                 tempList.push(newOne);
+                var n = Number.parseInt(number);
+                n++;
+                number = n.toString();
+                // console.log("created object newOne: " + newOne.num);
+                // console.log(
+                //   "tempList affter addition: " +
+                //     tempList.forEach((data) => console.log(data))
+                // );
             }
-            prev = prev + tempList.length;
-            nodeList.push.apply(nodeList, tempList);
-            console.log("Afrer push apply" + nodeList + nodeList.length);
-            nodeList.forEach((data) => {
-                console.log("Num" + data.num);
-                data.linkedArray.forEach((data) => {
-                    console.log(data);
-                });
-            });
-            tempList = [];
         }
-        depth--;
-        console.log(depth + "after decriment");
+        var tempListSize = tempList.length;
+        console.log("size: " + tempListSize);
+        nodeList.push.apply(nodeList, tempList);
+        tempList = [];
+        depth++;
     }
+    // console.log("final list:");
+    nodeList.forEach((data) => console.log(data));
 }
 exports.test = test;
 test();
--- src/testAlgorithm.ts
+++ src/testAlgorithm.ts
@@ -1,46 +1,66 @@
 import { Edge } from "js-graph-algorithms";
 
 type TestTYpe = {
-  num: number;
-  linkedArray: Array<number>;
+  num: string;
+  linkedArray: Array<string>;
 };
 
-var firstOne: TestTYpe = { num: 1, linkedArray: [2, 3] };
+var firstOne: TestTYpe = { num: "1", linkedArray: ["1", "1", "1"] };
 
 export function test() {
-  var depth = 1;
-  console.log(depth);
+  //temp value
+  var number = "2";
+  var depth = 0;
+  var tempListSize = 0;
+
+  // console.log(depth);
+
   var nodeList: Array<TestTYpe> = [firstOne];
   var tempList: Array<TestTYpe> = [];
-  while (depth != 0) {
-    var prev = 0;
-    var current = nodeList.length;
-    console.log(prev);
-    for (var edge in nodeList.slice(prev)) {
+
+  while (depth != 3) {
+    // console.log("depth: " + depth);
+
+    for (var edge in nodeList.slice(-tempListSize)) {
       //   var nodeEdge = nodeList[edge];
       //   nodeEdge.label = "first";
       // получить список всех путей из кода
+
       var extractedRoutes = nodeList[edge].linkedArray;
+      // console.log("extractedRoute: " + nodeList[edge].linkedArray);
       for (var route in extractedRoutes) {
+        // console.log("route: " + route);
         // какая-то работа с путями
         // делаем edge | из edge -> route + weight
-        var newOne: TestTYpe = { num: 2, linkedArray: [4, 5] };
+
+        var newOne: TestTYpe = {
+          num: number,
+          linkedArray: [number, number, number],
+        };
         tempList.push(newOne);
+        var n = Number.parseInt(number);
+        n++;
+        number = n.toString();
+        // console.log("created object newOne: " + newOne.num);
+        // console.log(
+        //   "tempList affter addition: " +
+        //     tempList.forEach((data) => console.log(data))
+        // );
       }
-      prev = prev + tempList.length;
-      nodeList.push.apply(nodeList, tempList);
-      console.log("Afrer push apply" + nodeList + nodeList.length);
-      nodeList.forEach((data) => {
-        console.log("Num" + data.num);
-        data.linkedArray.forEach((data) => {
-          console.log(data);
-        });
-      });
-      tempList = [];
     }
-    depth--;
-    console.log(depth + "after decriment");
+
+    var tempListSize = tempList.length;
+
+    console.log("size: " + tempListSize);
+
+    nodeList.push.apply(nodeList, tempList);
+
+    tempList = [];
+    depth++;
   }
+
+  // console.log("final list:");
+  nodeList.forEach((data) => console.log(data));
 }
 
 test();
