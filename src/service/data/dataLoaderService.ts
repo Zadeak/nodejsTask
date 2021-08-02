caa44650659f11baef346e7aef80ea2f89a332a0
Mike Jevdokimov <jevdokimov.maik@gmail.com>
2021-08-02 21:10:30 +0300

    added counter back to help with data loading(

--- src/service/data/dataLoaderService.ts
+++ src/service/data/dataLoaderService.ts
@@ -1,10 +1,14 @@
+import { delay } from "../../helperfunctions";
 import { logger } from "../../logger";
-import { asyncWriteAirportsDataFromFile, asyncWriteRoutesDataFromFile, writeRoutesDao } from "./dataLoader";
+import { asyncWriteAirportsDataFromFile, asyncWriteRoutesDataFromFile, counter, writeRoutesDao } from "./dataLoader";
 
 
 export async function loadData(){
-    await asyncWriteAirportsDataFromFile();
     await asyncWriteRoutesDataFromFile();
+    while (counter < 67000) {
+      await delay(200);
+    }
+    await asyncWriteAirportsDataFromFile();
     await writeRoutesDao();
     logger.info("data is loaded");
 }
